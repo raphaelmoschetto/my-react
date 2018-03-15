e9b6eca1a43f3e98506a68ba2e3241daabd50fd9
Raphael Moschetto <raphael.moschetto@hotmail.com>
2018-03-14 22:54:33 -0300

    Creating Component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustom from './components/InputCustom';
 
 class App extends Component {
 
 	constructor(){
 		super();
-		this.state = {lista: []};
+		this.state = {lista: [],nome:'',email:'',senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
 	}
 
-	componentWillMount(){
+	componentDidMount(){
 	 	$.ajax({
 		      url:"http://cdc-react.herokuapp.com/api/autores",
 		      dataType: 'json',
@@ -21,6 +26,36 @@ class App extends Component {
 		  }
 		);
 	}
+
+  enviaForm(event)
+  {
+      event.preventDefault();
+      $.ajax({
+          url: 'http://cdc-react.herokuapp.com/api/autores',
+          contentType: 'application/json',
+          dataType: 'json',
+          type: 'post',
+          data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+          success: function(resposta){
+              this.setState({lista:resposta});
+          }.bind(this),
+          error: function(resposta){
+              console.log("error");
+          }
+      });
+  }
+
+  setNome(event){
+      this.setState({nome: event.target.value});
+  }
+
+  setEmail(event){
+      this.setState({email: event.target.value});
+  }
+
+  setSenha(event){
+      this.setState({senha: event.target.value});
+  }
 
   render() {
     return (
@@ -51,19 +86,12 @@ class App extends Component {
             </div>
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha"  />                                      
-                  </div>
+                <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+
+                  <InputCustom id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"></InputCustom>
+                  <InputCustom id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="E-mail"></InputCustom>
+                  <InputCustom id="senha" type="text" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"></InputCustom>
+                  
                   <div className="pure-control-group">                                  
                     <label></label> 
                     <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
@@ -84,7 +112,7 @@ class App extends Component {
                       { 
                       	this.state.lista.map(function(autor){
                       		return (
-                      			<tr>
+                      			<tr key={autor.id}>
                       				<td>
                       				{autor.nome}
                       				</td>
